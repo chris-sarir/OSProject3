5775f0d72e3957d422b922321860731e178cf5c6
Amir S <a.s.sarir@gmail.com>
2015-12-01 12:55:35 -0600

    Implements "write" server logic

--- src/utd/persistentDataStore/datastoreServer/commands/WriteCommand.java
+++ src/utd/persistentDataStore/datastoreServer/commands/WriteCommand.java
@@ -2,13 +2,25 @@ package utd.persistentDataStore.datastoreServer.commands;
 
 import java.io.IOException;
 
+import utd.persistentDataStore.utils.FileUtil;
 import utd.persistentDataStore.utils.ServerException;
+import utd.persistentDataStore.utils.StreamUtil;
 
 public class WriteCommand extends ServerCommand {
 
 	@Override
 	public void run() throws IOException, ServerException {
-		// TODO Auto-generated method stub
+
+		String name = StreamUtil.readLine(inputStream);
+		String amount = StreamUtil.readLine(inputStream);
+
+		byte[] data = StreamUtil.readData(Integer.parseInt(amount), inputStream);
+
+		FileUtil.writeData(name, data);
+
+		String response = "ok\n";
+
+		StreamUtil.writeLine(response, outputStream);
 
 	}
 
