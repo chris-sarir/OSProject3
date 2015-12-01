166ed55790a42eabbd8ad38f27bdf58160f7bccb
lxr131230 <lxr131230@CSLAB252832.campus.ad.utdallas.edu>
2015-12-01 13:38:16 -0600

    no longer needed for the project

--- src/utd/persistentDataStore/datastoreClient/ObjectstoreClient.java
+++
@@ -1,8 +0,0 @@
-package utd.persistentDataStore.datastoreClient;
-
-public interface ObjectstoreClient
-{
-	void writeObject(String name, Object object) throws ClientException;
-
-	Object readObject(String name) throws ClientException;
-}
