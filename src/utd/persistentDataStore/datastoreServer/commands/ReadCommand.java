5540ec5bca27530c321adf05e1aacbb6fd225cee
lxr131230 <leonardo.rocha1990@gmail.com>
2015-12-03 10:01:42 -0600

    implemented Read command

--- src/utd/persistentDataStore/datastoreServer/commands/ReadCommand.java
+++ src/utd/persistentDataStore/datastoreServer/commands/ReadCommand.java
@@ -2,14 +2,22 @@ package utd.persistentDataStore.datastoreServer.commands;
 
 import java.io.IOException;
 
+import utd.persistentDataStore.utils.FileUtil;
 import utd.persistentDataStore.utils.ServerException;
+import utd.persistentDataStore.utils.StreamUtil;
 
 public class ReadCommand extends ServerCommand {
 
 	@Override
 	public void run() throws IOException, ServerException {
-		// TODO Auto-generated method stub
-
+		
+		String name = StreamUtil.readLine(inputStream);
+		
+		byte[] data = FileUtil.readData(name);
+		
+		StreamUtil.writeData(data, outputStream);
+		
+		sendOK();
 	}
 
 }
